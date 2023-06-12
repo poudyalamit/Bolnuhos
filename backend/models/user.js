61fec58240888d52243c9ca07cb2014e9f553e07
poudyalamit <poudyalamit20@gmail.com>
2023-06-12 23:59:06 +0545

    Schema for the contents done

---
+++ backend/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const user = mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pic: {
+        type: String, required: true,
+        default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+},
+    {
+        timestamp: true
+    })
+
+
+const User= mongoose.model("User",user);
+
+module.exports=User;
